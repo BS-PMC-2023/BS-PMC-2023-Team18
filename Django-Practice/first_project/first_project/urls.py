c98a80e11ab5600fa11548ce7e316a3ea36d95bc
tomerda3 <tomerda3@ac.sce.ac.il>
2023-03-29 20:46:37 +0300

    Add new print

--- Django-Practice/first_project/first_project/urls.py
+++ Django-Practice/first_project/first_project/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include, url
+from first_app import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
 print("king vladis")
