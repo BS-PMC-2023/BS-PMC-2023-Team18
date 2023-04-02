e3050c658cae1364bdb993164bc57955b731ca58
tomerda3 <tomerda3@ac.sce.ac.il>
2023-04-02 18:26:38 +0300

    first project

--- rewear/Rewear/rewear_project/manage.py
+++ rewear/Rewear/rewear_project/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rewear_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,7 +17,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+
 if __name__ == '__main__':
     main()
-
-
--- rewear/Rewear/rewear_project/rewear_project/urls.py
+++ rewear/Rewear/rewear_project/rewear_project/urls.py
@@ -1,4 +1,4 @@
-"""first_project URL Configuration
+"""rewear_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,11 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include, url
-from first_app import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
-print("king vladis")
