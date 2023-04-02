bfc064b1711490854598130def458b8bf1ca3422
tomerda3 <tomerda3@ac.sce.ac.il>
2023-04-02 19:23:16 +0300

    setting up a main template

--- rewear/Rewear/rewear_project/rewear_app/views.py
+++ rewear/Rewear/rewear_project/rewear_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world")
+    my_dict = {'insert_me': "Hello I am from views.py!"}
+    return render(request, 'index.html', context=my_dict)
 
 # Create your views here.
--- rewear/Rewear/rewear_project/rewear_project/settings.py
+++ rewear/Rewear/rewear_project/rewear_project/settings.py
@@ -6,11 +6,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -51,7 +53,7 @@ ROOT_URLCONF = 'rewear_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
