d8cda10151b18e972676b2bd450772bdc6462299
tomerda3 <tomerda3@ac.sce.ac.il>
2023-04-02 18:59:34 +0300

    urls_init

--- rewear/Rewear/rewear_project/rewear_project/urls.py
+++ rewear/Rewear/rewear_project/rewear_project/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from rewear_app import views
+from django.conf.urls import include
+from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^', views.index, name='index'),
+    url(r'^rewear_app/',include('rewear_app.urls')),
 ]
