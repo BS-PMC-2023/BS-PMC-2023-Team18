9406a3c012b74f5766979a02160f2f1d2c9ffc72
tomerda3 <tomerda3@ac.sce.ac.il>
2023-04-18 20:17:22 +0300

    fixing menu

--- rewear/Rewear/rewear_project/main/admin.py
+++ rewear/Rewear/rewear_project/main/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 # from .models import ToDoList, Item
 # from .models import HostingPlace, GuideInfo
-from main.models import market
+from main.models import market, submission
 # Register your models here.
 # admin.site.register(ToDoList)
 # admin.site.register(Item)
 admin.site.register(market)
+admin.site.register(submission)
 # admin.site.register(HostingPlace)
 # admin.site.register(GuideInfo)
