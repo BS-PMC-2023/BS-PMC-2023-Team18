96d5af58cd4fe76fd36242b98ee74a45f1c12926
maayanrab <maayara@ac.sce.ac.il>
2023-05-08 17:55:00 +0300

    Messaging system

--- rewear/Rewear/rewear_project/main/admin.py
+++ rewear/Rewear/rewear_project/main/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 # from .models import ToDoList, Item
 # from .models import HostingPlace, GuideInfo
-from main.models import market, submission, myEvent
+from main.models import market, submission, myEvent, Message
 # Register your models here.
 # admin.site.register(ToDoList)
 # admin.site.register(Item)
@@ -10,3 +10,4 @@ admin.site.register(submission)
 admin.site.register(myEvent)
 # admin.site.register(HostingPlace)
 # admin.site.register(GuideInfo)
+admin.site.register(Message)
--- rewear/Rewear/rewear_project/main/models.py
+++ rewear/Rewear/rewear_project/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django import forms
 
 # Create your models here.
@@ -44,3 +45,11 @@ class myEvent(models.Model):
     market_id = models.CharField(max_length=10, default='')
     def __str__(self):
         return str(self.id)
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    subject = models.CharField(max_length=255)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
