09c36cbf2cbcd798d25867d8df376715432c7ff2
maayanrab <maayara@ac.sce.ac.il>
2023-05-09 18:59:38 +0300

    event manager can edit amount
    event manager can edit the amount of clothing items.
    
    BSPMC2318-15
    
    git commit -m "BSPMC2318-15 <message>"
    
    BSPMC2318-17
    
    git commit -m "BSPMC2318-17 <message>"

--- rewear/Rewear/rewear_project/main/views.py
+++ rewear/Rewear/rewear_project/main/views.py
@@ -207,6 +207,30 @@ def update_market(response, id):
                   {'market': cur_market, 'new_mail': new_messages(response.user.username)})
 
 
+def set_market_value(response, id):
+    cur_market = market.objects.get(id=id)
+    if response.method == 'POST':
+        shirt = int(response.POST['shirt'])
+        pants = int(response.POST['pants'])
+        shoes = int(response.POST['shoes'])
+        hat = int(response.POST['hat'])
+        gloves = int(response.POST['gloves'])
+        scarf = int(response.POST['scarf'])
+        jacket = int(response.POST['jacket'])
+        # add to the database
+        cur_market.shirt = shirt
+        cur_market.pants = pants
+        cur_market.shoes = shoes
+        cur_market.hat = hat
+        cur_market.gloves = gloves
+        cur_market.scarf = scarf
+        cur_market.jacket = jacket
+        cur_market.save()
+        return market_page(response, id)
+    return render(response, "main/market_page.html",
+                  {'market': cur_market, 'new_mail': new_messages(response.user.username)})
+
+
 def assign_manager(response, mid, username):
     cur_market = market.objects.get(id=mid)
     print(cur_market.market_manager)
