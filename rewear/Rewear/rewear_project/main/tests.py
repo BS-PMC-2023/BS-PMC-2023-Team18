c8fc5220b45056d5ddb7634f3de499fe34f64e93
maayanrab <maayara@ac.sce.ac.il>
2023-05-09 19:44:12 +0300

    unit tests

--- rewear/Rewear/rewear_project/main/tests.py
+++ rewear/Rewear/rewear_project/main/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase, Client
-from django.urls import reverse
-from django.contrib.auth.models import User
-from registry.models import UserProfileInfo
-from .models import market, submission
-from .views import *
+# from django.urls import reverse
+# from django.contrib.auth.models import User
+# from registry.models import UserProfileInfo
+# from .models import market, submission
+# from .views import *
 
 class Test(TestCase):
 
@@ -12,29 +12,9 @@ class Test(TestCase):
         response = self.client.get('//')
         self.assertEqual(response.status_code, 200)
 
-    #this one is okay
+    # this one is okay
     def test_about(self):
         response = self.client.get('/about/')
-        self.assertEqual(response.status_code, 200)
-
-    # def test_areyousure(self):
-    #    response = self.client.get('/areyousure/')
-    #    self.assertEqual(response.status_code, 200)
-
-    # def test_messagetouser(self):
-    #     response = self.client.get('/messagetouser/')
-    #     self.assertEqual(response.status_code, 200)
-
-    # def test_sendmessage(self):
-    #     response = self.client.get('/sendmessage/')
-    #     self.assertEqual(response.status_code, 200)
-
-    # def test_editabout(self):
-    #     response = self.client.get('/editabout/')
-    #     self.assertEqual(response.status_code, 200)
-    #
-    def test_saveabout(self):
-        response = self.client.get('/saveabout/')
         self.assertEqual(response.status_code, 200)
 
     # this one is okay
@@ -47,44 +27,23 @@ class Test(TestCase):
         response = self.client.get('/search_page/')
         self.assertEqual(response.status_code, 200)
 
-    # def test_market_page(self):
-    #     response = self.client.get('/market_page/')
-    #     self.assertEqual(response.status_code, 200)
-
-    # def test_update_market(self):
-    #     response = self.client.get('/update_market/')
-    #     self.assertEqual(response.status_code, 200)
-    #
     # this one is okay
     def test_submissions(self):
         response = self.client.get('/submissions/')
         self.assertEqual(response.status_code, 200)
 
+    def test_send_message(self):
+        response = self.client.get('/send_message/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_inbox(self):
+        response = self.client.get('/inbox/')
+        self.assertEqual(response.status_code, 302)
+
     def test_submit_request(self):
-        self.user = User.objects.create_user(username='testuser', password='testpass')
-        self.market = Market.objects.create(name='testmarket', address='testaddress')
-        self.user.save()
-        self.market.save()
-        url = reverse('submit_request', args=[self.user.id, self.market.id])
-        response = self.client.get(url)
+        response = self.client.get('/submit_request/2/2/')
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'submit_request.html')
-        self.assertContains(response, 'Submit Request')
-        self.user.delete()
-        self.market.delete()
 
-    def test_feedback(self):
-        response = self.client.get('/feedback/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_update_profilepic(self):
-        response = self.client.get('/update_profilepic/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_getUserProfileInfo(self):
-        response = self.client.get('/getUserProfileInfo/')
-        self.assertEqual(response.status_code, 404)
-
-    def test_toggle_active(self):
-        response = self.client.get('/toggle_active/')
+    def test_message_detail(self):
+        response = self.client.get('/message_detail/0/')
         self.assertEqual(response.status_code, 404)
