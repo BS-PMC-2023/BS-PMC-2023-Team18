f3fc1b1fcbbb510e7b5a9b2c5413c23cae75de9f
maayanrab <maayara@ac.sce.ac.il>
2023-05-16 20:34:57 +0300

    comments

--- rewear/Rewear/rewear_project/main/views.py
+++ rewear/Rewear/rewear_project/main/views.py
@@ -80,19 +80,6 @@ def toggle_active(response):
     return home(response)
 
 
-def sendmessage(response, username):
-    if response.method == 'POST':
-        message = response.POST['message']
-        message = message + "\n\nMy email: " + User.objects.get(username=response.user.username).email
-        send_mail('Rewear: A new message from ' + str(response.user.username),
-                  message,
-                  settings.EMAIL_HOST_USER,
-                  [str(User.objects.get(username=username).email)],
-                  fail_silently=False)
-    new_mail = new_messages(response.user.username)
-    return render(response, 'main/thankyou2.html', {'new_mail': new_mail})
-
-
 def about(response):
     new_mail = new_messages(response.user.username)
     return render(response, "main/about.html", {'new_mail': new_mail})
@@ -287,20 +274,7 @@ def submit_request(response, uid, mid):
     return render(response, "main/submissions.html", {'submissions': submissions, 'new_mail': new_mail})
 
 
-# user story 14 - Market FeedBack
-"""def feedback(response, id):
-    if response.method == 'POST':
-        message = response.POST['message']
-        send_mail('Contact Form',
-                  message,
-                  settings.EMAIL_HOST_USER,
-                  ['Rewear100@gmail.com'],
-                  fail_silently=True)
-    cur_market = market.objects.get(id=id)
-    new_mail = new_messages(response.user.username)
-    return render(response, "main/market_page.html", {'market': cur_market, 'feedback': True, 'new_mail': new_mail})"""
-
-
+# user story 14 - Market FeedBack, also general feedback
 def feedback(response, market_name):
     cur_market = None
     if response.method == 'POST':
